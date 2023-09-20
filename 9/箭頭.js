1d84f1d3de039b8e22b7417683182e54757c8fae
逸 <j093519941070@gmail.com>
2023-09-20 17:01:11 +0800

    小改

--- 9/箭頭.js
+++ 9/箭頭.js
@@ -9,3 +9,10 @@ const sum = (x, y) => {
 const add = function(x,y){
     return x + y;
 }
+// 把=> 後面大誇 改成小誇 可以顯示
+// 只有一個值 跟返回才能用簡短式
+// 還可以省掉return
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+const add = (a, b) => a + b
