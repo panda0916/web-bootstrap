5e0564c4510aacf53074ba83b48bad54b2869581
逸 <j093519941070@gmail.com>
2023-09-22 23:23:03 +0800

    de

---
+++ 9/destructuring.js
@@ -0,0 +1,8 @@
+const scores = [1, 2, 3, 4, 5, 6, 7, 8]
+// 顯示出來num是1
+const num = scroes[0]
+// 顯示出來num1是2
+const num1 = scroes[1]
+// 更簡短的寫法
+const [num, num1, num2] =scores;
+// 顯示出來是 num=1 num1=2 num2=3
