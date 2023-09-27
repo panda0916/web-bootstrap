4b87a73925d2c35237301e7946583ba4bea3d4a8
逸 <j093519941070@gmail.com>
2023-09-27 15:40:32 +0800

    更

--- 9/000.js
+++
@@ -1,8 +0,0 @@
-const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-
-for (let i = 1; 1 < 10; i++) {
-    const newImg = document.createElement('img')
-    newImg.src = `${baseURL}${i}.png`
-    container.appendChild(newImg)
-}
---
+++ 9/pokemon.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('#container');
+const base = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+for (let i = 1; i <= 151; i++) {
+    const pokemon = document.createElement('div')
+    pokemon.classList.add('pokemon')
+    const label = document.createElement('span')
+    label.innerText = `#${i}`
+    const newImg = document.createElement('img')
+    newImg.src = `${base}${i}.png`
+    pokemon.appendChild(newImg)
+    pokemon.appendChild(label)
+    container.appendChild(pokemon)
+}
